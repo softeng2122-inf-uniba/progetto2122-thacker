60562cd08b42ab093be78565c75c500e4c2410f3
Piersilvio <p.spicoli@studenti.uniba.it>
2022-05-19 18:31:51 +0200

    correzione del bug per Gradle

--- src/test/java/it/uniba/app/AppTest.java
+++ src/test/java/it/uniba/app/AppTest.java
@@ -1,14 +1,4 @@
 package it.uniba.app;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import org.junit.jupiter.api.Test;
-
 public class AppTest {
-    
-    @Test
-    public void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(
-                "app should have a greeting", classUnderTest.getGreeting());
-    }
 }
