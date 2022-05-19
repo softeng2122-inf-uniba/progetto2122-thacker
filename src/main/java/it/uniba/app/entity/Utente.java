55bb0a4fb8cc6ca24fa7d4a598e647c5a0922d8e
ilenia <sasanelli.ilenia@gmail.com>
2022-05-19 11:21:58 +0200

    stesura della classe utente

---
+++ src/main/java/it/uniba/app/entity/Utente.java
@@ -0,0 +1,65 @@
+package it.uniba.app.entity;
+
+public class Utente {
+    private Comando comando;
+    private String username;
+    private int id;
+
+    public Utente(String username, int id) {
+        this.username = username;
+        this.id = id;
+    }
+
+    public Utente(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Comando getComando() {
+        return comando;
+    }
+
+    public void setComando(Comando comando) {
+        this.comando = comando;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Utente other = (Utente) obj;
+        if (id != other.id)
+            return false;
+        return true;
+    }
+
+    
+   
+}
