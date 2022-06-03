432fc63e596264652fb4f0917dea23a5bbc45753
raffaella <r.ricciardi15@studenti.uniba.it>
2022-06-03 18:38:29 +0200

    Correzione degli errori di checkstyle Giocatore.java

--- src/main/java/it/uniba/app/entity/Giocatore.java
+++ src/main/java/it/uniba/app/entity/Giocatore.java
@@ -3,53 +3,105 @@ package it.uniba.app.entity;
 import it.uniba.app.control.Controllo;
 
 public class Giocatore extends Utente {
+    /**
+     * nTentativi : numero di tentativi massimp affidati al giocatore
+     */
     private int nTentativi;
+    /**
+     * parola_Giocatore : parola inserita dall'utente
+     */
     private String parola_Giocatore;
+    /**
+     * controllo : oggetto controllo
+     */
     private Controllo controllo;
 
-    public Giocatore(int id){
+    /**
+     *
+     * @param id :id dell'utente
+     */
+    public Giocatore(final int id) {
         super(id);
-        nTentativi=-1;
+        nTentativi = -1;
     }
 
-    public Giocatore(String username, int id, int nTentativi, String parola_Giocatore) {
+    /**
+     *
+     * @param username : usermname dell'utente
+     * @param id : id dell'utente
+     * @param nTentativi : numero di tentativi massimo del giocatore
+     * @param parola_Giocatore : parola inserita dal giocatore
+     */
+    public Giocatore(final String username, final int id, final int nTentativi,  final String parola_Giocatore) {
         super(username, id);
         this.nTentativi = nTentativi;
         this.parola_Giocatore = parola_Giocatore;
     }
 
-    public Giocatore(int id, int nTentativi, String parola_Giocatore) {
+    /**
+     *
+     * @param id : id dell'utente
+     * @param nTentativi : numero dei tentativi massimo concesso al''utente
+     * @param parola_Giocatore : parola inserita dal giocatore
+     */
+    public Giocatore(final int id, final int nTentativi, final String parola_Giocatore) {
         super(id);
         this.nTentativi = nTentativi;
         this.parola_Giocatore = parola_Giocatore;
     }
 
-    public int getnTentativi() {
+    /**
+     *
+     * @return : ritorna il numero di tentativi
+     */
+    public final int getnTentativi() {
         return nTentativi;
     }
 
-    public void setnTentativi(int nTentativi) {
+    /**
+     *
+     * @param nTentativi : ritorna il numero di tentativi
+     */
+    public final void setnTentativi(final int nTentativi) {
         this.nTentativi = nTentativi;
     }
 
-    public String getParola_Giocatore() {
+    /**
+     *
+     * @return : la parola del giocatore
+     */
+    public final String getParola_Giocatore() {
         return parola_Giocatore;
     }
 
-    public void setParola_Giocatore(String parola_Giocatore) {
+    /**
+     *
+     * @param parola_Giocatore : la parola del giocatore
+     */
+    public final void setParola_Giocatore(final String parola_Giocatore) {
         this.parola_Giocatore = parola_Giocatore;
     }
 
-    public Controllo getControllo() {
+    /**
+     *
+     * @return : oggetto di tipo controllo
+     */
+    public final Controllo getControllo() {
         return controllo;
     }
 
-    public void setControllo(Controllo controllo) {
+    /**
+     *
+     * @param controllo : oggetto controllo
+     */
+    public final void setControllo(final Controllo controllo) {
         this.controllo = controllo;
     }
-   
-    
-    public void incrTentativi(){
+
+    /**
+     * incrementa il numero di tentativi
+     */
+    public final void incrTentativi() {
         nTentativi++;
     }
 }
